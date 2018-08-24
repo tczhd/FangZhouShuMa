7f3ba307e0bd0ee80cdd323f67ddbe1bd4884313
david <hongdingzhu@hotmail.com>
2018-08-23 22:04:58 -0700

    Add project files.

---
+++ FangZhouShuMa.Migrations/Migrations/20180824045850_initTables.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FangZhouShuMa.Migrations.Migrations
+{
+    public partial class initTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Slot",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FormId = table.Column<int>(type: "int", nullable: false),
+                    PeopleNeed = table.Column<int>(type: "int", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18, 2)", nullable: true),
+                    SignupId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Slot", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Slot");
+        }
+    }
+}
