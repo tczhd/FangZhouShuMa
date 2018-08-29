d706e381338e9d3125589ce3e9223539584e158e
david <hongdingzhu@hotmail.com>
2018-08-28 22:39:24 -0700

    Create user role

--- FangZhouShuMa.Web/Startup.cs
+++ FangZhouShuMa.Web/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using FangZhouShuMa.DataAccess;
 using FangZhouShuMa.DataAccess.Models;
 using FangZhouShuMa.Web.Services;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace FangZhouShuMa.Web
 {
@@ -46,7 +47,7 @@ namespace FangZhouShuMa.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -69,6 +70,53 @@ namespace FangZhouShuMa.Web
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            CreateUserRoles(serviceProvider).Wait();
+        }
+
+        private async Task CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            IdentityResult roleResult;
+            //Adding Admin Role
+            var roleCheck = await RoleManager.RoleExistsAsync("Admin");
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            roleCheck = await RoleManager.RoleExistsAsync("Manager");
+            // creating Creating Manager role    
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Manager"));
+            }
+
+            roleCheck = await RoleManager.RoleExistsAsync("Employee");
+            // creating Creating Employee role    
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Employee"));
+            }
+
+            roleCheck = await RoleManager.RoleExistsAsync("Customer");
+            // creating Creating Customer role    
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Customer"));
+            }
+
+            //Assign Admin role to the main User here we have given our newly registered 
+            //login id for Admin management
+            //ApplicationUser user = await UserManager.FindByEmailAsync("David@orderbot.com");
+            //var User = new ApplicationUser();
+            //await UserManager.AddToRoleAsync(user, "Admin");
         }
     }
 }
