580e5aa0cf291fae5cbaa057b2671e5db3ae5c14
david <hongdingzhu@hotmail.com>
2018-09-03 00:30:01 -0700

    Check in code

---
+++ FangZhouShuMa.Web/Models/CatalogViewModels/CatalogViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FangZhouShuMa.Web.Models.ProductViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FangZhouShuMa.Web.Models.CatalogViewModels
+{
+    public class CatalogViewModel
+    {
+        public IEnumerable<ProductViewModel> Items { get; set; }
+
+    }
+}
