49e4773f573aa4d57870cabb42012abc6d94040d
david <hongdingzhu@hotmail.com>
2018-09-06 23:33:39 -0700

    check in code

--- FangZhouShuMa.ApplicationCore/Models/ProductReport/ProductCustomFieldOptionModel.cs
+++ FangZhouShuMa.ApplicationCore/Models/ProductReport/ProductCustomFieldOptionModel.cs
@@ -8,6 +8,7 @@ namespace FangZhouShuMa.ApplicationCore.Models.ProductReport
 {
     public class ProductCustomFieldOptionModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Sequence { get; set; }
--- FangZhouShuMa.Infrastructure/Data/Reports/Product/ProductReport.cs
+++ FangZhouShuMa.Infrastructure/Data/Reports/Product/ProductReport.cs
@@ -1,25 +1,42 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using FangZhouShuMa.ApplicationCore.Models.ProductReport;
 using FangZhouShuMa.ApplicationCore.Utilities;
 using FangZhouShuMa.Infrastructure.Data.Reports.Product.Filters;
 using FangZhouShuMa.Infrastructure.Interfaces.Product;
 
 namespace FangZhouShuMa.Infrastructure.Data.Reports.Product
 {
-    public class ProductReport : IReport<ProductReport, ProductFilter>
+    public class ProductReport :  IReport<ProductReport, ProductFilter>
     {
-        public int SalesChannelId { get; set; }
-        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public DateTime ProductCreateDateUTC { get; set; }
+        public DateTime ProductLastUpdateDateUTC { get; set; }
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public int ProductCustomFieldId { get; set; }
+        public string ProductCustomFieldName { get; set; }
 
+        public decimal ProductCustomFieldPrice { get; set; }
+
+        public int ProductCustomFieldFieldTypeId { get; set; }
+        public int OptionId { get; set; }
+        public string OptionName { get; set; }
+        public decimal OptionPrice { get; set; }
+        public int OptionSequence { get; set; }
         public IQueryable<ProductReport> GenerateQuery(FangZhouShuMaContext context, ProductFilter filter, Sort<ProductReport> sortBy = null)
         {
             var query = context.Products.AsQueryable();
 
             var result = query.Select(m => new ProductReport
             {
-                SalesChannelId = m.Id,
+                ProductId = m.Id,
 
-                Name = m.Name
+
             });
 
             return result;
