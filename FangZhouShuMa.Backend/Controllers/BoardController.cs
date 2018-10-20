a815364ee2d4faf063deb7ae27021fbe573afd77
david <hongdingzhu@hotmail.com>
2018-10-19 23:27:45 -0700

    check in code

---
+++ FangZhouShuMa.Backend/Controllers/BoardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FangZhouShuMa.Backend.Controllers
+{
+    [Route("Board")]
+    public class BoardController : Controller
+    {
+        [Route("{view=Index}")]
+        public IActionResult Index(string view)
+        {
+            ViewData["Title"] = "主面板";
+
+            return View(view);
+        }
+    }
+}
